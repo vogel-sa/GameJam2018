130726f027c068fab3d3f5c39249b3633c181eda
svogel <svogel@ccs.neu.edu>
2018-01-27 10:37:05 -0500

    Added enemy follow behavior, new physics layers, and level colliders for the test level

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 1f;
+	private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit2D rc;
+		if (rc = Physics2D.Raycast (transform.position,
+			MovePlayer.Instance.currPlayer.transform.position - transform.position,
+			Mathf.Infinity,
+				LayerMask.GetMask("Wall", "Character"))) {
+			if (rc.collider.gameObject == MovePlayer.Instance.currPlayer.gameObject)
+				rb.velocity = (MovePlayer.Instance.currPlayer.transform.position - transform.position).normalized * speed;
+			else
+				rb.velocity = Vector2.zero;
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawRay(new Ray(transform.position,
+			MovePlayer.Instance.currPlayer.transform.position - transform.position));
+	}
+}
