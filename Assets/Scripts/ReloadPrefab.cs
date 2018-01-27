17179ae5009abd98ddbd24dafdacd438a2e9b382
svogel <svogel@ccs.neu.edu>
2018-01-27 17:30:38 -0500

    Added level reload

---
+++ Assets/Scripts/ReloadPrefab.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReloadPrefab : MonoBehaviour {
+	private static ReloadPrefab _instance;
+	public static ReloadPrefab Instance
+	{
+		get
+		{
+			if (!_instance && !(_instance = FindObjectOfType<ReloadPrefab>()))
+			{
+				_instance = new GameObject().AddComponent<ReloadPrefab>();
+			}
+			return _instance;
+		}
+	}
+
+	private GameObject goRef = null;
+
+	[SerializeField]
+	string path;
+
+	public void CreatePrefab()
+	{
+		goRef = GameObject.Instantiate (Resources.Load<GameObject> (path));
+	}
+
+	public void Reload()
+	{
+		GameObject.Destroy(goRef);
+		CreatePrefab();
+	}
+
+	void Awake()
+	{
+		CreatePrefab ();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Reload ();
+		}
+	}
+}
