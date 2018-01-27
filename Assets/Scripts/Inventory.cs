8de51b26b206c782cb00cbcd81bc566ae752a246
Ryan Teng <ryanrteng@gmail.com>
2018-01-27 01:32:33 -0500

    Inventory and storeable

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+	
+    public GameObject item;
+	public Sprite itemImage;
+    private static Inventory _instance;
+    public static Inventory Instance
+    {
+        get
+        {
+            if (!_instance && !(_instance = FindObjectOfType<Inventory>()))
+            {
+                _instance = new GameObject().AddComponent<Inventory>();
+            }
+            return _instance;
+        }
+    }
+	//Adds the item to the inventory, if the inventory is not empty, return the item that was in the inventory
+    public void addItem(GameObject toAdd)
+	{
+        
+		item = toAdd;
+        SpriteRenderer rend;
+        if(rend = toAdd.GetComponent<SpriteRenderer>())
+            itemImage = rend.sprite;
+	}
+
+    public void removeItem()
+    {
+
+        item.SetActive(true);
+        if()
+        item.gameObject.transform.position = MovePlayer.Instance.currPlayer.transform.position;
+        item = null;
+
+    }
+}
---
+++ Assets/Scripts/Storeable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Storeable : MonoBehaviour
+{
+    bool storable = false;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Collider2D>().isTrigger = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.E)){
+            gameObject.SetActive(false);
+            Inventory.Instance.addItem(gameObject);
+            //TODO Store the item
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject == MovePlayer.Instance.currPlayer.gameObject){
+            storable = true;
+
+        }
+
+    }
+    void OnTriggerExit2D(Collider2D other){
+        if(other.gameObject == MovePlayer.Instance.currPlayer.gameObject){            
+            storable = false;
+
+        }
+
+    }
+}
